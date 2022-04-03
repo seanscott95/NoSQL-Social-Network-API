2e28b4db607d4f21c1137cadeb76868c35122394
Sean <seanms418@gmail.com>
2022-04-03 21:01:38 +1000

    added create thought route needs some work, added update and delete thought routes, added empty create and delete reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -20,4 +20,50 @@ router.get('/:thoughtId', (req, res) => {
         .catch((err) => res.status(500).json(err));
 });
 
+// Creates a new thought
+// push the created thought's `_id` to the associated user's `thoughts` array field
+// router.post('/', (req, res) => {
+//     Thought.create(req.body)
+//         .then((thought) => res.json(thought))
+//         .catch((err) => res.status(500).json(err))
+// });
+
+// Updates a thought
+router.put('/:thoughtId', (req, res) => {
+    Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+        .then((thought) => 
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID!'})
+                : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+});
+
+// Deletes a thought
+router.delete("/:thoughtId", (req, res) => {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) => {
+            !thought
+                ? res.status(404).json({ message: 'No thought with that ID' })
+                : res.json(thought)
+                
+        })
+        .then(() => res.json({ message: 'Thought successfully deleted!' }))
+        .catch((err) => res.status(500).json(err));
+});
+
+// Creates a reaction
+// router.post("/:thoughtId/reactions", (req, res) => {
+
+// })
+
+// Deletes a reaction
+// router.delete("/:thoughtId/reactions", (req, res) => {
+
+// })
+
 module.exports = router;
