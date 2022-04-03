3fa6af47a75ac9d2174267817dd3203e106837be
Sean <seanms418@gmail.com>
2022-04-03 20:38:42 +1000

    added the create, update and delete user routes

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
-//Gets all users
+// Gets all users
 router.get('/', (req, res) => {
     User.find()
         .then((users) => res.json(users))
@@ -9,14 +9,49 @@ router.get('/', (req, res) => {
 });
 
 // Gets a single user
-router.get('/single', (req, res) => {
-    User.findOne({ _id: req.params.userId})
+router.get('/:userId', (req, res) => {
+    User.findOne({ _id: req.params.userId })
         .select('-__v')
-        .then((user) => 
+        .then((user) =>
             !user
                 ? res.status(404).json({ message: 'No user with that ID' })
                 : res.json(user)
         )
         .catch((err) => res.status(500).json(err));
-})
+});
 
+// Creates a new user
+router.post('/', (req, res) => {
+    User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err))
+});
+
+// Updates a user
+router.put('/:userId', (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+    )
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID!'})
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+});
+
+// Deletes a user
+router.delete("/:userId", (req, res) => {
+    User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) => {
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+        })
+        .then(() => res.json({ message: 'User successfully deleted!' }))
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
