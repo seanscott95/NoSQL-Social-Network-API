272a1cbb8ad4b03c420bfa9ec8eb04de11b1e094
Sean <seanms418@gmail.com>
2022-04-03 18:02:35 +1000

    created the user model, virtual frined count and set up for export of the model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username required"],
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (v) {
+                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+            required: [true, "Email required"]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ],
+
+    },
+    {
+        toJSON: {
+            virtauls: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtaul("friendCount", {
+    ref: 'friends',
+    count: true
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
