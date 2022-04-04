fa31e9e44d599dea0192edb6e4c1d08e49aa48a9
Sean <seanms418@gmail.com>
2022-04-04 18:03:30 +1000

    turned the reaction into a reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,37 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
-const thoughtSchema = new mongoose.Schema(
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: [true, "Reaction required"],
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: [true, "Username required"],
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => {
+                return date.toDateString()
+            },
+        },
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
@@ -19,29 +50,7 @@ const thoughtSchema = new mongoose.Schema(
             type: String,
             required: [true, "Username required"],
         },
-        reactions: {
-            reactionId: {
-                type: Schema.Types.ObjectId,
-                default: () => new Types.ObjectId(),
-            },
-            reactionBody: {
-                type: String,
-            required: [true, "Reaction required"],
-            minlength: 1,
-            maxlength: 280,
-            },
-            username: {
-                type: String,
-                required: [true, "Username required"],
-            },
-            createdAt: {
-                type: Date,
-                default: Date.now,
-                get: (date) => {
-                    return date.toDateString()
-                },
-            },
-        }
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
