c865471414e6e7b85e3b43d8de9549af6879390c
Sean <seanms418@gmail.com>
2022-04-04 17:32:18 +1000

    renamed the api routes to kebab style

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const usersRoutes = require('./usersRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const usersRoutes = require('./users-routes');
+const thoughtRoutes = require('./thought-routes');
 
 router.use('/users', usersRoutes);
 router.use('/thoughts', thoughtRoutes);
