6e276ffc4a12622763038484813fe1c9dd1775e1
Sean <seanms418@gmail.com>
2022-04-04 17:21:24 +1000

    added friends and thoughts to populate when getting a single and all users

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -4,6 +4,14 @@ const { User, Thought } = require('../../models');
 // Gets all users
 router.get('/', (req, res) => {
     User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err))
 });
@@ -11,6 +19,14 @@ router.get('/', (req, res) => {
 // Gets a single user
 router.get('/:userId', (req, res) => {
     User.findOne({ _id: req.params.userId })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
         .select('-__v')
         .then((user) =>
             !user
